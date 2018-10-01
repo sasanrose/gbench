8de9861eaacdf4d96d478c3f9577114e4c80c0d9
Sasan Rose <sasan.rose@gmail.com>
2018-10-01 14:54:29 +1000

    Fix cmd and render package lint problems

--- render/renderer.go
+++ render/renderer.go
@@ -1,7 +1,10 @@
+// Package render defines the Renderer interface to be implemented by different
+// renderer drivers.
 package render
 
 import "github.com/sasanrose/gbench/report"
 
+// Renderer defines renderer interface.
 type Renderer interface {
 	Render(result *report.Result) error
 }
